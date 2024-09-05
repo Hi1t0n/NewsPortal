04a7ce303917a3f42c2dd135dffcd35a3e3abe33
HILTON <artyomhilton@yandex.ru>
2024-09-06 02:08:40 +0300

    Add files

---
+++ Backend/UserService/UserService.Host/Program.cs
@@ -0,0 +1,11 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.Run();
