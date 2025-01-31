14b5e7a0d8570fb8767b2cf34bac3e763ac1b343
HILTON <artyomhilton@yahoo.com>
2025-01-31 03:16:58 +0300

    Added swagger

--- Backend/PostService/PostService.Host/Program.cs
+++ Backend/PostService/PostService.Host/Program.cs
@@ -7,9 +7,20 @@ var connectionStringDb = builder.Environment.IsDevelopment()
     ? builder.Configuration.GetConnectionString(ConnectionStrings.ConnectionStringDb)!
     : Environment.GetEnvironmentVariable(ConnectionStrings.ConnectionStringDb)!;
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddBusinessLogic(connectionStringDb);
 
 var app = builder.Build();
 
+if (builder.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
+}
 
 app.Run();
